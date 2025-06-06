62e104644db8752718cfb52661034bc93b3d7dc2
LyudmilaKostanyan <lyudakstnn@gmail.com>
2025-06-06 17:25:32 +0400

    Add inline function benchmarks

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 #include <vector>
 #include <chrono>
 
-int main() {
+void test_optimization()
+{
 	const size_t size = 100000000;
 	std::vector<int> data(size, 1);
 
@@ -18,6 +19,61 @@ int main() {
 
 	std::cout << "Sum: " << sum << "\n";
 	std::cout << "Elapsed time: " << elapsed.count() << " seconds\n";
+}
 
+inline int add_inline(int a, int b) __attribute__((always_inline));
+inline int add_inline(int a, int b) {
+    return a + b;
+}
+
+inline int multiply_inline(int a, int b) __attribute__((always_inline));
+inline int multiply_inline(int a, int b) {
+    return a * b;
+}
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
+void test_inline()
+{
+	volatile int result = 0;
+	auto start = std::chrono::high_resolution_clock::now();
+
+	for (int i = 0; i < 100000000; ++i) {
+		result += add(i, i);
+		result += multiply(i, i);
+	}
+
+	auto end = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> diff = end - start;
+
+	std::cout << "Result: " << result << "\n";
+	std::cout << "Elapsed time: " << diff.count() << " seconds\n";
+
+	result = 0;
+	start = std::chrono::high_resolution_clock::now();
+
+	for (int i = 0; i < 100000000; ++i) {
+		result += add_inline(i, i);
+		result += multiply_inline(i, i);
+	}
+
+	end = std::chrono::high_resolution_clock::now();
+	diff = end - start;
+
+	std::cout << "Result with inline: " << result << "\n";
+	std::cout << "Elapsed time with inline: " << diff.count() << " seconds\n";
+}
+
+int main()
+{
+	test_optimization();
+	test_inline();
 	return 0;
 }
+
