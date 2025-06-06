5ebd5d76a58f2629f346f4604439759a53a9eeb8
LyudmilaKostanyan <lyudakstnn@gmail.com>
2025-06-06 19:35:43 +0400

    Add noinline attribute to functions

--- main.cpp
+++ main.cpp
@@ -28,44 +28,53 @@ void test_optimization()
 #endif
 
 FORCE_INLINE int add_inline(int a, int b) {
-    return a + b;
+	return a + b;
 }
 
 FORCE_INLINE int multiply_inline(int a, int b) {
-    return a * b;
+	return a * b;
 }
 
+#if defined(_MSC_VER)
+  #define NOINLINE __declspec(noinline)
+#elif defined(__GNUC__) || defined(__clang__)
+  #define NOINLINE __attribute__((noinline))
+#else
+  #define NOINLINE
+#endif
 
-int add(int a, int b) {
-    return a + b;
+NOINLINE int add(int a, int b) {
+	return a + b;
 }
 
-int multiply(int a, int b) {
-    return a * b;
+NOINLINE int multiply(int a, int b) {
+	return a * b;
 }
 
 void test_inline_vs_no_inline() {
-    volatile int result = 0;
-    const int N = 100000000;
+	volatile int result = 0;
+	const int N = 100000000;
 
-    auto start = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < N; ++i) {
-        result += add(i, i);
-    }
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> diff_no_inline = end - start;
+	auto start = std::chrono::high_resolution_clock::now();
+	for (int i = 0; i < N; ++i) {
+		result += add(i, i);
+		multiply(i, i);
+	}
+	auto end = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> diff_no_inline = end - start;
 
-    result = 0;
-    start = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < N; ++i) {
-        result += add_inline(i, i);
-    }
-    end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> diff_inline = end - start;
+	result = 0;
+	start = std::chrono::high_resolution_clock::now();
+	for (int i = 0; i < N; ++i) {
+		result += add_inline(i, i);
+		multiply_inline(i, i);
+	}
+	end = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> diff_inline = end - start;
 
-    std::cout << "No inline time: " << diff_no_inline.count() << " s\n";
-    std::cout << "Inline time: " << diff_inline.count() << " s\n";
-    std::cout << "Result: " << result << "\n";
+	std::cout << "No inline time: " << diff_no_inline.count() << " s\n";
+	std::cout << "Inline time: " << diff_inline.count() << " s\n";
+	std::cout << "Result: " << result << "\n";
 }
 
 int main()
